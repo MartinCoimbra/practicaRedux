6226ad71eb52295e6f63b729649257bfb3c6772f
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-11-22 14:46:11 -0300

    todo list map

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const initialState = {
@@ -9,22 +10,39 @@ export const reducer = (state = initialState, action) => {
     case "todo/add": {
       return {
         ...state,
-        entities: [{}],
+        entities: state.entities.concat({ ...action.payload }),
       };
     }
   }
   return state;
 };
 
+const TodoItem = ({ todo }) => {
+  return <li>{todo.title}</li>;
+};
+
 const App = () => {
+  const [value, setValue] = useState("");
   const dispatch = useDispatch();
   const state = useSelector((x) => x);
-  console.log(state);
+
+  console.log({ state });
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!value.trim()) {
+      return;
+    }
+    const id = Math.random().toString(36);
+    const todo = { title: value, completed: false, id };
+    dispatch({ type: "todo/add", payload: todo });
+    setValue("");
+  };
 
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={submit}>
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
       </form>
       <button onClick={() => dispatch({ type: "todo/add" })}>
         Mostrar todos
@@ -32,8 +50,9 @@ const App = () => {
       <button>Completados</button>
       <button>Incompletados</button>
       <ul>
-        <li>Todo 1</li>
-        <li>Todo 2</li>
+        {state.entities.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
       </ul>
     </div>
   );
